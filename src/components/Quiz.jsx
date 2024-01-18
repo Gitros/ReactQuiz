ef886a7424745c63960c81086a558c0903c98ec1
Gitros <31899790+Gitros@users.noreply.github.com>
2024-01-18 17:48:19 +0100

    timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
+import quizCompleteImage from '../assets/quiz-complete.png'
+
+import QuestionTimer from "./QuestionTimer.jsx";
 import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -13,12 +17,25 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImage} alt="quiz completed" />
+                <h2>Quiz completed</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
